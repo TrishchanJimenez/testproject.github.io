dd7ea23a698e7fe0a2c4305cd270036235286213
TrishchanJimenez <trishchanjimenez06@gmail.com>
2023-12-22 19:11:44 +0800

    Added the calculation feature

--- calculator.js
+++ calculator.js
@@ -34,16 +34,51 @@ function toggleTheme(elements, newDigit) {
 
 const key_btn = [...document.getElementsByClassName('key')];
 var currentValue = "";
+var pressedOperator = false;
+var firstNumber;
+var secondNumber;
+var operator;
+var result;
 key_btn.forEach(element => {
     element.addEventListener('click', () => {
-        if(element.value == "DEL") {
+        const value = element.value;
+        if(value == "DEL") {
             currentValue = currentValue.slice(0, -1);
         }
-        else if(element.value == "RESET") {
+        else if(value == "RESET") {
             currentValue = "";
+            pressedOperator = false;
+            firstNumber = 0;
+            secondNumber = 0;
+        }
+        else if (value == "+" || value == "-" || value == "x" || value == "/" && currentValue != "") {
+            operator = value;
+            firstNumber = Number(currentValue);
+            pressedOperator = true;
+            currentValue = "";
+            console.log(firstNumber);
+        }
+        else if(value == "=" && pressedOperator) {
+            secondNumber = Number(currentValue);
+            switch(operator) {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "x":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+            }
+            currentValue = result;
+            firstNumber = result;
         }
         else {
-            currentValue += element.value;
+            currentValue += value;
         }
         screenText.value = currentValue;
     });
